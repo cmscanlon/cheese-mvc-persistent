c8e7cef8a6247dc27ee79931915f04f546b74a8c
Carolyn Scanlon <cmscanlon@yahoo.com>
2019-09-10 11:45:44 -0500

    working through third part

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -52,6 +53,7 @@ public class CheeseController {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Cheese");
+            model.addAttribute("categories", categoryDao.findAll());
             return "cheese/add";
         }
 
@@ -77,5 +79,15 @@ public class CheeseController {
 
         return "redirect:";
     }
+
+//    @RequestMapping(value = "category", method = RequestMethod.GET)
+//    public String category(Model model, @RequestParam int id) {
+//
+//        Category cat = categoryDao.findOne(id);
+//        List<Cheese> cheeses = cat.getCheeses();
+//        model.addAttribute("cheeses", cheeses);
+//        model.addAttribute("title", "Cheeses in Category: ", + cat.getName() );
+//        return "cheese/index";
+//    }
 
 }
--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -12,39 +12,33 @@ public class Category {
     @Id
     @GeneratedValue
     private int id;
-    private String name;
 
     @NotNull
     @Size(min=3, max=15)
-    public String getName() {
-        return name;
-    }
+    private String name;
 
     @OneToMany
     @JoinColumn(name = "category_id")
     private List<Cheese> cheeses = new ArrayList<>();
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getId() {
         return id;
     }
 
-    public Category() {
-    }
+    public Category() { }
 
     public Category(String name) {
         this.name = name;
     }
 
+    public String getName() {
+    return name;
+}
+
+    public void setName(String name) { this.name = name; }
+
     public List<Cheese> getCheeses() {
         return cheeses;
-    }
-
-    public void setCheeses(List<Cheese> cheeses) {
-        this.cheeses = cheeses;
     }
 
 }
